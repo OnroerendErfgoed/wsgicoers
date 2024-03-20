12336e9723f3dbff8ab4bbb9af02ac413341b5aa
Tim Van Campenhout <tim.vancampenhout@realdolmen.com>
2024-03-20 10:46:04 +0100

    #3 remove lru cache requirement

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
-#-*- coding:utf-8 -*-
-from setuptools import setup
-import sys, os
+# -*- coding:utf-8 -*-
 import codecs
+import os
+import sys
+from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
 
+
 def readfile(fname):
     return codecs.open(os.path.join(here, fname), encoding='utf-8').read()
+
 
 version = '0.10.0'
 
@@ -14,11 +17,8 @@ README = readfile('README.rst')
 CHANGES = readfile('CHANGES.rst')
 AUTHORS = readfile('AUTHORS.rst')
 
-install_requires=[]
+install_requires = []
 
-if sys.version_info < (3, 2):
-    install_requires=["backports.functools_lru_cache >= 1.0"]
-    
 # hack, or test wont run on py2.7
 try:
     import multiprocessing
@@ -29,31 +29,31 @@ except:
 setup(name='wsgicoers',
       version=version,
       description="WSGI for Cross Origin Resource Sharing (CORS)",
-      long_description=README + '\n\n' +  CHANGES + '\n\n' +  AUTHORS,
-      classifiers = [
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
-        "Development Status :: 3 - Alpha",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Internet :: WWW/HTTP :: WSGI"
-        ],
+      long_description=README + '\n\n' + CHANGES + '\n\n' + AUTHORS,
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 3",
+          "Development Status :: 3 - Alpha",
+          "Environment :: Web Environment",
+          "Intended Audience :: Developers",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Topic :: Internet :: WWW/HTTP :: WSGI"
+      ],
       keywords=["wsgi", "cors"],
       author='Norman Krämer',
       author_email='kraemer.norman@googlemail.com',
       url="https://github.com/may-day/wsgicors",
       license='Apache Software License 2.0',
       py_modules=["wsgicoers"],
-      install_requires = install_requires,
-      tests_require = [
-        'nose',
-        'nose-testconfig',
-        'webob'
-        ],
-      test_suite = 'nose.collector',
-      entry_points = """
+      install_requires=install_requires,
+      tests_require=[
+          'nose',
+          'nose-testconfig',
+          'webob'
+      ],
+      test_suite='nose.collector',
+      entry_points="""
       [paste.filter_app_factory]
       middleware = wsgicoers:make_middleware
       """
